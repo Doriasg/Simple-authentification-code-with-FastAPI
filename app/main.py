03fc67f20fe1c0a73de34162af69dd3c0f9892cc
Doriane ASSOGBA <assogbadoriane3@gmail.com>
2026-04-26 22:57:45 +0100

    inclusion du fichier auth.routers après pull

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@ def on_startup():
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(pictures.router)
-app.include_router(auth.router)
 
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
