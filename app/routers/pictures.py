d0f0884d66129f0d6805eea2d774da584749d573
Doriane ASSOGBA <assogbadoriane3@gmail.com>
2026-04-29 18:05:13 +0100

    sauvegarde des images

--- app/routers/pictures.py
+++ app/routers/pictures.py
@@ -1,29 +1,23 @@
-from fastapi import APIRouter, Depends, UploadFile, File, Form
+from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException
 from sqlmodel import Session
-import os
-import uuid
 from datetime import datetime
 from typing import Annotated
 from app.database import get_db
 from app.models import PlantImage, Users
 from app.dependencies import get_current_user
-from app.schemas import UploadImageRequest
 import cloudinary
 import cloudinary.uploader
+import os
 
 cloudinary.config(
-    cloud_name="dwtzbiklb",
-    api_key="638477191621292",
-    api_secret="qe8pS8odtkH1I2DWEr9xd06SG_Q"
+    cloud_name=os.getenv("CLOUD_NAME"),
+    api_key=os.getenv("API_KEY"),
+    api_secret=os.getenv("API_SECRET")
 )
-
 
 router = APIRouter()
 
 SessionDep = Annotated[Session, Depends(get_db)]
-
-UPLOAD_DIR = "uploads"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 @router.post("/upload")
@@ -31,26 +25,33 @@ def save_plant_image(
     session: SessionDep,
     current_user: Users = Depends(get_current_user),
     file: UploadFile = File(...),
-    disease_name : str = Form(None)
-    
+    disease_name: str = Form(...)
 ):
-    
-    result = cloudinary.uploader.upload(file.file)
+    try:
+        result = cloudinary.uploader.upload(
+            file.file,
+            folder="plants",
+            resource_type="image"
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=500,
+            detail="Erreur lors de l'upload de l'image"
+        )
+
     image_url = result["secure_url"]
 
-    # 2. création DB (EN DEHORS DU WITH)
     plant = PlantImage(
         image_path=image_url,
         user_id=current_user.id,
         created_at=datetime.utcnow(),
-        disease_name= disease_name
+        disease_name=disease_name
     )
 
     session.add(plant)
     session.commit()
     session.refresh(plant)
 
-    # 3. response
     return {
         "message": "Image uploadée avec succès",
         "id": plant.id,
