b68830993b9b6c27a28c228a36de501dd1066a5e
Doriane ASSOGBA <assogbadoriane3@gmail.com>
2026-04-29 18:49:37 +0100

    sauvegarde des images

--- app/routers/pictures.py
+++ app/routers/pictures.py
@@ -1,29 +1,20 @@
-from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException
+from fastapi import APIRouter, Depends, UploadFile, File, Form
 from sqlmodel import Session
+import os
+import uuid
 from datetime import datetime
 from typing import Annotated
 from app.database import get_db
 from app.models import PlantImage, Users
 from app.dependencies import get_current_user
-import cloudinary
-import cloudinary.uploader
-import os
+from app.schemas import UploadImageRequest
 
-if not all([
-    os.getenv("CLOUD_NAME"),
-    os.getenv("API_KEY"),
-    os.getenv("API_SECRET")
-]):
-    raise RuntimeError("Variables Cloudinary manquantes")
-
-cloudinary.config(
-    cloud_name=os.getenv("CLOUD_NAME"),
-    api_key=os.getenv("API_KEY"),
-    api_secret=os.getenv("API_SECRET")
-)
 router = APIRouter()
 
 SessionDep = Annotated[Session, Depends(get_db)]
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 @router.post("/upload")
@@ -31,33 +22,28 @@ def save_plant_image(
     session: SessionDep,
     current_user: Users = Depends(get_current_user),
     file: UploadFile = File(...),
-    disease_name: str = Form(...)
+    disease_name : str = Form(None)
+    
 ):
-    try:
-        result = cloudinary.uploader.upload(
-            file.file,
-            folder="plants",
-            resource_type="image"
-        )
-    except Exception:
-        raise HTTPException(
-            status_code=500,
-            detail="Erreur lors de l'upload de l'image"
-        )
-
-    image_url = result["secure_url"]
-
+    ext = file.filename.split(".")[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    file_path = os.path.join(UPLOAD_DIR, filename)
+    # 1. sauvegarde fichier
+    with open(file_path, "wb") as buffer:
+        buffer.write(file.file.read())
+    # 2. création DB (EN DEHORS DU WITH)
     plant = PlantImage(
-        image_path=image_url,
+        image_path=file_path,
         user_id=current_user.id,
         created_at=datetime.utcnow(),
-        disease_name=disease_name
+        disease_name= disease_name
     )
 
     session.add(plant)
     session.commit()
     session.refresh(plant)
 
+    # 3. response
     return {
         "message": "Image uploadée avec succès",
         "id": plant.id,
